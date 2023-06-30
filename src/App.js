8539e17adf2bd8e0d388518eae2cbf74fcf1e7c7
Elizabeth Doyle <myoddjourney@gmail.com>
2023-06-30 18:26:09 -0400

    fixed useEffect hook, and added roles in form

--- src/App.js
+++ src/App.js
@@ -3,11 +3,38 @@ import axios from 'axios';
 import Form from './components/Form'
 import './App.css';
 
+const teamMembers = {
+  name: "",
+  email: "",
+  role: ""
+}
+
 function App() {
+
+  const {members, setMembers}= useState('')
+  const {formValues, setFormValues} = useState(teamMembers)
+
+
+  const onChange = () => {
+
+  }
+
+  useEffect( () => {
+    axios.get('fakeapi.com')
+    .then(res => setMembers(res.data))
+    .catch(err => console.error(err))
+  }, [])
+
+
+
+
+
   return (
     <div className="App">
       This is the App rendering
-    <Form />
+    <Form 
+      value={formValues}
+     />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-const Form = () => {
+const Form =(props) => {
+ 
+    const {values, submit } = props;
+
+
+    
+  const onSubmit = (evt) => {
+    evt.preventDevault()
+  }
 
 
 
     return (
-        <div>THIS IS THE FORM page returning</div>
+     <div>THIS IS THE FORM page returning
+        <form className="form" >
+            <input 
+            name={props.name}
+            type="text"
+            // value={}
+            // onChange={}
+
+            
+            
+            />
+            <input  
+                email={props.email}
+                type="text"
+            />
+
+            <select value={props.role} name="role" >
+               <option value="" >Select A Role</option>
+               <option value="Student" >Student</option>
+               <option value="Instructor" >Instructor</option>
+               <option value="Alumni" >Alumni</option>
+          </select>
+
+            <submit type="button" >Submit</submit>
+        </form>
+        
+        
+    `</div>
     )
 }
 
